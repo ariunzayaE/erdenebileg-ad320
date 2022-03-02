d040d753a3fad6f4b5bbfc5e1f697934b54c6bc6
arerde <arerde@uw.edu>
2022-03-01 23:25:18 -0800

    started week8 assignment

---
+++ client/src/components/FlashCard/CreateFlashcard.js
@@ -0,0 +1,92 @@
+import React, {useState} from "react"
+import { Button, Stack, TextField } from "@mui/material"
+import axios from 'axios'
+
+const CreateFlashcard = ({ userId, deckId }) => {
+  // how can we use state here to make sure we're validating info
+  console.log(`[CreateFlashcard] deckId is ${deckId}`)
+  const [formValue, setFormValue] = useState({})
+
+  const isUrl = (value) => {
+    const re = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+    return re.test(value)
+  }
+
+  const validateFormValue = (userInput) => {
+   return (
+     userInput.frontImage && 
+     userInput.backImage && 
+     userInput.frontText && 
+     userInput.backText && 
+     isUrl(userInput.frontImage) && 
+     isUrl(userInput.backImage))
+  }
+
+  const handleChange = (event) => {
+    event.preventDefault()
+    console.log("[CreateFlashcard] onChange ", event)
+    console.log(validateFormValue(formValue), "validation status")
+    const currentValues = formValue
+    currentValues[event.target.name] = event.target.value
+    setFormValue(currentValues)
+  }
+  
+  const handleSubmit = async (event) => {
+    console.log("[CreateFlashcard] onSubmit ", event)
+    event.preventDefault()
+    try {
+      const response = await axios.post(`http://localhost:8000/decks/${deckId}/cards`, formValue, { headers: { user: userId } })
+      console.log(`[createflashcard] response submit ${response.status}`)
+    } catch (err) {
+      console.log(`response error ${err.status}`)
+    }
+  }
+
+  return (
+    <Stack component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+      <span>Form values: {formValue.frontText} &amp; {formValue.backText}</span>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="frontImage"
+        label="Front Image"
+        name="frontImage"
+        onChange={handleChange}
+        autoFocus
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="frontText"
+        label="Front Text"
+        id="frontText"
+        onChange={handleChange}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="backImage"
+        label="Back Image"
+        name="backImage"
+        onChange={handleChange}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        name="backText"
+        label="Back Text"
+        id="backText"
+        onChange={handleChange}
+      />
+      <Button type="submit" disabled={validateFormValue(formValue) ? true : false} fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Submit
+      </Button>
+    </Stack>
+  )
+}
+
+export default CreateFlashcard
